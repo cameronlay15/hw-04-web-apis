c701e8f1c9a65a3e08957eb7c5ec69ddaa9e517b
Cameron Lay <cameronlay15@gmail.com>
2020-09-08 16:26:06 -0400

    added questions and css

---
+++ script.js
@@ -0,0 +1,43 @@
+var questions =[
+    {
+    Question: "Commonly used data types DO NOT Include:"
+    answer1: "strings"
+    answer2: "booleans"
+    correctAnswer1: "alerts"
+    answer4: "numbers"
+},
+{
+    Question: "The condition in an if/else statement is enclosed within:"
+    answer1: "quotes"
+    answer2: "curly brackeets"
+    correctAnswer2: "parenthesis"
+    answer4: "square brackets"
+},
+{
+    Question: "Arrays in JavaScript can be used to store:"
+    answer1: "Numbers and strings"
+    answer2: "other arrays"
+    Answers3: "booleans"
+    correctAnswer3: "all of the above"
+},
+{
+    Question: "String values must be enclosed withing ____ when being assigned to variables:"
+    answer1: "commas"
+    answer2: "curly brackets"
+    CorrectAnswer4: "quotes"
+    Answer4: "parenthesis"
+},
+{
+    Question: "A very useful tool used during development and debugging for printing content to the debugger is:"
+    answer1: "JavaScript"
+    answer2: "terminal/bash"
+    Answers3: "for loops"
+    correctAnswer5: "console.log"
+},
+
+
+
+
+
+
+]
